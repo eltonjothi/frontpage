5ec0206b0d065fd2ee65cfa5772871255d628340
Elton Jothi <elton.jothi@edgeprop.my>
2020-02-02 03:02:21 +0800

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+
+import '../styles/style.scss';
+
+function HomePage() {
+  return (
+    <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
+  );
+}
+
+export default HomePage

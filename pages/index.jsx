ac104573bb583fb68db39122e2caca274b2c464e
Elton Jothi <elton.jothi@edgeprop.my>
2020-02-10 01:02:10 +0800

    added layout

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,9 +4,61 @@ function HomePage() {
   return (
     <div class="container">
       <div class="row">
-        <div class="col-sm">One of three columns</div>
-        <div class="col-sm">One of three columns</div>
-        <div class="col-sm">One of three columns</div>
+        <div class="col-md-4">
+          <div class="row">
+            <div class="col-12">
+              <h2>Elton Jothi</h2>
+              <h3>Front End Engineer</h3>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+                odio dolor, aliquet non massa sed, cursus congue mi. In hac
+                habitasse platea dictumst.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-8">
+          <div class="row">
+            <div class="col-12">
+              <h3>Experience</h3>
+              <h4>EdgeProp.my / Frontend Engineer</h4>
+              <h5>Oct 2018 - Present</h5>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+              </ul>
+            </div>
+            <div class="col-12">
+              <h4>OpenTech Software / Web Developer</h4>
+              <h5>Oct 2018 - Present</h5>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+              </ul>
+            </div>
+            <div class="col-12">
+              <h4>Navodaya Media Works / Software Engineer</h4>
+              <h5>Oct 2018 - Present</h5>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

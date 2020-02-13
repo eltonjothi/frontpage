b5fe8260db3acde7ad5be08e478856495452e899
Elton Jothi <elton.jothi@edgeprop.my>
2020-02-13 23:18:45 +0800

    real content

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,9 +14,9 @@ function HomePage() {
           <div class="row">
             <div class="col-12">
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                odio dolor, aliquet non massa sed, cursus congue mi. In hac
-                habitasse platea dictumst.
+                To work with a team where I can utilize my skills, knowledge,
+                ideas and contribute to the development of new technologies and
+                products as a software developer.
               </p>
             </div>
           </div>
@@ -26,35 +26,36 @@ function HomePage() {
             <div class="col-12">
               <h3>Experience</h3>
               <h4>EdgeProp.my / Frontend Engineer</h4>
-              <h5>Oct 2018 - Present</h5>
+              <h5>2018 - Present</h5>
               <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Morbi leo risus</li>
-                <li class="list-group-item">Porta ac consectetur ac</li>
-                <li class="list-group-item">Vestibulum at eros</li>
+                <li class="list-group-item">Translation of designer mock-ups and wireframes into front-end code</li>
+                <li class="list-group-item">Optimizing the website to improve page speed and user experience across all devices</li>
+                <li class="list-group-item">Maintaining standard code style with shareable configs within the project to improve the efficiency and code readability across team.</li>
+                <li class="list-group-item">Wrote support documentation and provided training to the team internally.</li>
+                <li class="list-group-item">Ensuring SEO best practices are properly implemented.</li>
+                <li class="list-group-item">Managed project development through version control systems.</li>
               </ul>
             </div>
             <div class="col-12">
               <h4>OpenTech Software / Web Developer</h4>
-              <h5>Oct 2018 - Present</h5>
+              <h5>2016 - 2018</h5>
               <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Morbi leo risus</li>
-                <li class="list-group-item">Porta ac consectetur ac</li>
-                <li class="list-group-item">Vestibulum at eros</li>
+                <li class="list-group-item">Optimized front-end efficiency by shifting the blogs over to WordPress and brand-associated them to the main site.</li>
+                <li class="list-group-item">Managed hosting through AWS and helped securing the applications.</li>
+                <li class="list-group-item">Created custom plugins and integrating the web application with Marketing Solutions</li>
+                <li class="list-group-item">Increasing website performance using SEO best practices.</li>
+                <li class="list-group-item">Marketing Strategies that resulted in 40% increase in sales revenue.</li>
               </ul>
             </div>
             <div class="col-12">
               <h4>Navodaya Media Works / Software Engineer</h4>
-              <h5>Oct 2018 - Present</h5>
+              <h5>2014 - 2015</h5>
               <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Morbi leo risus</li>
-                <li class="list-group-item">Porta ac consectetur ac</li>
-                <li class="list-group-item">Vestibulum at eros</li>
+                <li class="list-group-item">Developed mobile games using cocos2dx and unity3d.</li>
+                <li class="list-group-item">Managed project development through version control systems.</li>
+                <li class="list-group-item">Coordinated with team of UX designers to create new Web Portal to host games online.</li>
+                <li class="list-group-item">Administration and management of a hosting environment</li>
+                <li class="list-group-item">Increasing website performance using SEO best practices.</li>
               </ul>
             </div>
           </div>

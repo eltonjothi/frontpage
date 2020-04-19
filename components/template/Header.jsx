452a9501a14b3a39439d4b9ee6a72615104a3057
Elton Jothi <elton.jothi@edgeprop.my>
2020-04-19 22:33:45 +0800

    comment dark mode for later

--- components/template/Header.jsx
+++ components/template/Header.jsx
@@ -3,55 +3,56 @@ import Link from 'next/link';
 
 function Header() {
   return (
-    <div className="navbar-container">
-      <nav className="navbar navbar-expand-lg navbar-dark">
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mx-auto">
-            <li className="nav-item active">
-              <Link href="/">
-                <a className="nav-link">/home</a>
-              </Link>
-            </li>
-            <li className="nav-item active">
-              <Link href="/resume">
-                <a className="nav-link">/resume</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                /blog
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-              >
-                (dark)
-              </a>
-            </li>
-          </ul>
+    <>
+      <nav className="navbar sticky-top navbar-expand-lg navbar-dark">
+        <div className="container">
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item active">
+                <Link href="/">
+                  <a className="nav-link">/home</a>
+                </Link>
+              </li>
+              <li className="nav-item active">
+                <Link href="/resume">
+                  <a className="nav-link">/resume</a>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  /contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* <button className="btn btn-outline-success my-2 my-sm-0">dark</button> */}
         </div>
       </nav>
       <nav className="mobile-navbar">
         <div className="mobile-navbar-container">
           <div className="mobile-navbar-item">
-          <Link href="/"><a>/home</a></Link>
+            <Link href="/">
+              <a>/home</a>
+            </Link>
           </div>
           <div className="mobile-navbar-item">
-          <Link href="/resume"><a>/resume</a></Link>
+            <Link href="/resume">
+              <a>/resume</a>
+            </Link>
           </div>
           <div className="mobile-navbar-item">
-          <Link href="/"><a>/contact</a></Link>
+            <Link href="/">
+              <a>/contact</a>
+            </Link>
           </div>
-          <div className="mobile-navbar-item">
-          <Link href="/"><a>/blog</a></Link>
-          </div>
+          {/* <div className="mobile-navbar-item">
+            <Link href="/">
+              <a>/blog</a>
+            </Link>
+          </div> */}
         </div>
       </nav>
-    </div>
+    </>
   );
 }
 

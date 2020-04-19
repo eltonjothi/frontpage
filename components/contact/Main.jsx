50f3da82673d15b2b6348e048dcfa1695d5272b8
Elton Jothi <elton.jothi@edgeprop.my>
2020-04-19 22:52:55 +0800

    added contact page

--- components/contact/Main.jsx
+++ components/contact/Main.jsx
@@ -2,34 +2,12 @@ import React from 'react';
 
 function Main() {
   return (
-    <div className="container">
+    <div className="container contact-body">
       <div className="row justify-content-center">
-        <div className="col-12 col-lg-8">
-          <div className="intro-hero">
-            <div className="avatar">
-              <img
-                src="/images/elton-avatar-3.jpg"
-                className="profile-avatar"
-                alt="..."
-              />
-            </div>
-            <div className="details">
-              <h2>Hey there.! 👋</h2>
-              <p className="ft-200 grey-30">
-                My name is Elton Jothi. I’m an experienced, passionate,
-                well-rounded frontend engineer with full stack capabilities and
-                an eye for design.
-              </p>
-              <br />
-              <p className="ft-200 grey-30">
-                Currently at EdgeProp.my, building beautiful user interfaces
-                with a focus on performance and writing code with best
-                practices.
-              </p>
-            </div>
-          </div>
+        <div className="col-12 text-center">
+          <h2>I'm always happy to hear from you.</h2>
         </div>
-        <div className="col-12 col-lg-8">
+        <div className="col-12  text-center">
           <div>
             <ul className="social-links">
               <li>
@@ -60,17 +38,17 @@ function Main() {
                 </a>
               </li>
               <li>
-                  <a href="https://twitter.com/eltonjothi" target="_blank">
-                    <svg
-                      role="img"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title>Twitter icon</title>
-                      <path d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z" />
-                    </svg>
-                  </a>
-                </li>
+                <a href="https://twitter.com/eltonjothi" target="_blank">
+                  <svg
+                    role="img"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <title>Twitter icon</title>
+                    <path d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z" />
+                  </svg>
+                </a>
+              </li>
             </ul>
           </div>
         </div>
--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,12 +1,12 @@
 import '../styles/style.scss';
 import React from 'react';
 import Layout from '../components/template/Layout';
-import Main from '../components/resume/Main';
+import Main from '../components/contact/Main';
 
 function Contact() {
   return (
     <Layout classProp="contact">
-      <p>contact</p>
+      <Main />
     </Layout>
   );
 }

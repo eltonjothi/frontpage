3513752701764c57d6df249548874211e13354cd
Elton Jothi <elton.jothi@edgeprop.my>
2021-01-22 01:53:26 +0800

    added analytics

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,22 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-B0T85CBWC2"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-B0T85CBWC2');
+          `,
+          }}
+        />
+
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600&display=swap"
